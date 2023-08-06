6218a7d13068428fd7b3596b113281f5e7c1c1c2
Adrian Saycon <adrian@mycustomsoftware.com>
2023-08-06 23:25:49 +0800

    first commit

---
+++ wc-box.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Box Plugin
+ * Plugin URI: http://mycustomsoftware.com/
+ * Description: An extension to WooCommerce that displays how full a box is based on the size and weight of the items in the box.
+ * Version: 1.0.0
+ * Author: MCS
+ * Author URI: http://mycustomsoftware.com/
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * WC requires at least: 3.0.0
+ * WC tested up to: 5.9.0
+ * Text Domain: wc-box
+ */
+
+// If this file is called directly, abort.
+if (!defined('ABSPATH')) {
+    exit;
+}
