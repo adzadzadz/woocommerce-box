98701568a22c20397efac64ea6cd9f9a6dd1b750
Adrian Saycon <adrian@mycustomsoftware.com>
2023-08-11 05:22:47 +0800

    Simple product input points

--- includes/Admin_Product.php
+++ includes/Admin_Product.php
@@ -10,9 +10,34 @@ class Admin_Product extends Base
         ['woocommerce_variation_options', 'add_variation_options', 20, 3],
         // save custom fields for product variations
         ['woocommerce_save_product_variation', 'save_custom_field_variations', 10, 2],
+        // adding custom fields to simple product
+        ['woocommerce_product_options_general_product_data', 'add_simple_product_custom_fields'],
+        // save custom fields for simple product
+        ['woocommerce_process_product_meta', 'save_custom_field_simple_product', 10, 1],
         //  enqueuing scripts and styles
         ['admin_enqueue_scripts', 'enqueue_admin_scripts', 10, 1],
     ];
+
+    // add custom fields to simple product
+    public function add_simple_product_custom_fields()
+    {
+        woocommerce_wp_text_input(
+            [
+                'id' => 'wcb_item_points',
+                'type' => 'number',
+                'wrapper_class' => 'wcb_item_points_wrapper',
+                'class' => 'wcb_item_points_input-field',
+                'label' => 'Box item Points',
+                'description' => 'Enter the point value for this item',
+                'desc_tip' => true,
+                'value' => get_post_meta(get_the_ID(), 'wcb_item_points', true),
+                'custom_attributes' => [
+                    'step' => 'any',
+                    'min' => '0',
+                ],
+            ]
+        );
+    }
 
     public function enqueue_admin_scripts($hook)
     {
@@ -34,12 +59,22 @@ class Admin_Product extends Base
                 'wrapper_class' => 'wcb_item_points_wrapper',
                 'class' => 'wcb_item_points_input-field',
                 'custom_attributes' => ['data-variation_id' => $variation->ID, 'data-variation_loop' => $loop],
-                'label' => __('Box item points', 'woocommerce'),
+                'label' => 'Box item points',
                 'desc_tip' => 'true',
-                'description' => __('Enter the point value', 'woocommerce'),
+                'description' => 'Enter the point value',
                 'value' => get_post_meta($variation->ID, 'wcb_item_points', true)
             ]
         );
+    }
+
+    // save custom fields for simple product
+    public function save_custom_field_simple_product($post_id)
+    {
+        $wcb_item_points = $_POST['wcb_item_points'];
+        error_log(print_r($wcb_item_points, true));
+        if (isset($wcb_item_points)) {
+            update_post_meta($post_id, 'wcb_item_points', esc_attr($wcb_item_points));
+        }
     }
 
     public function save_custom_field_variations($variation_id, $i)
